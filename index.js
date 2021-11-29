29d05b242ab9aec4d57b430d02e547f8020d7489
Josh Marroquin <mj.marroquin993@gmail.com>
2021-11-29 15:27:45 -0800

    commit tree 44c0c8bef06418c62d98be7467cadc2ce2c50c2b

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 10)
+
+let num3 = 9
+let num4 = 5
+let mod = num3 % num4
+let max = Math.max(1, 13, 9, 20, 15, 8)
